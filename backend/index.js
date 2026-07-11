f0c93cbac1e14a9e4c19a02f7994ee97c57cd85d
menabil <nabil99cc@gmail.com>
2026-07-11 11:49:14 +0600

    connect mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const mongoss = require("mongoose");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.json());
 
-
-
+mongoose
+  .connect(
+    "mongodb+srv://root:admin123@practice.4u2mmnp.mongodb.net/class08?appName=Practice",
+  )
+  .then(() => console.log("Connected to MongoDB"));
 
 app.listen(5000, () => {
   console.log("Server is Running");
