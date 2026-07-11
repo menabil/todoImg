c079fa1dddbc2cdf18daae6de2272c7167cb8e4b
menabil <nabil99cc@gmail.com>
2026-07-11 11:43:59 +0600

    connect backend server

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,13 @@
 const express = require("express");
-
+const cors = require("cors");
+const mongoss = require("mongoose");
 const app = express();
+
+app.use(express.json());
+
+
+
+
+app.listen(5000, () => {
+  console.log("Server is Running");
+});
